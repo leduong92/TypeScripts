ba7c1efaf0efc43fdab1a827f7e97e85d8c4d236
Mai Le Duong <maileduong92@gmail.com>
2020-11-09 11:40:09 +0700

    01-basic-types

---
+++ TypeScripts/wwwroot/scripts/01-basic-types.js
@@ -0,0 +1,14 @@
+let isDone = false;
+let decimal = 6;
+let hex = 0xf00d;
+let color = "blue";
+color = "red";
+function revert(s) {
+    return s.split("").reverse().join("");
+}
+console.log(revert("hello world"));
+console.log(color);
+console.log(hex);
+console.log(decimal);
+console.log(isDone);
+//# sourceMappingURL=01-basic-types.js.map
---
+++ TypeScripts/wwwroot/scripts/01-basic-types.ts
@@ -0,0 +1,18 @@
+﻿let isDone: Boolean = false;
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+
+let color: string = "blue";
+color = "red";
+
+function revert(s: String): String {
+    return s.split("").reverse().join("");
+}
+
+console.log(revert("hello world"));
+
+console.log(color);
+console.log(hex);
+console.log(decimal);
+console.log(isDone);
