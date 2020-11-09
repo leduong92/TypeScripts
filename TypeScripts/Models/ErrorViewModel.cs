26000396104156010aee7b2cca6ac057220824c9
Mai Le Duong <maileduong92@gmail.com>
2020-11-09 11:27:37 +0700

    Add project files.

---
+++ TypeScripts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TypeScripts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
